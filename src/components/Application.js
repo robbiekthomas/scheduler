815173e9a15275813f175a36923874467ffa4fbd
Robbie Thomas <robbiekt@gmail.com>
2023-02-21 15:40:22 -0800

    add api request for daylist days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -63,6 +46,13 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8001/api/days")
+      .then((res) => setDays(res.data));
+  }, []);
 
   const appointmentList = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
