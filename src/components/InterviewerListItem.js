44c3ee6982810478f1d81b2cd73a661e1d5cd7dd
Robbie Thomas <robbiekt@gmail.com>
2023-02-21 08:31:21 -0800

    create interviewer list item componenet

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+const InterviewerListItem = (props) => {
+  const listClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={listClass}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
